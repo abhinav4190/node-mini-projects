c4afb7ed0d87043fabd96ec4580eb42a66c1c24c
abhinav <abhinavsinghabhi56@gmail.com>
2025-05-08 01:27:09 +0530

    jwt-authenticated-endpoint

---
+++ jwt-authenticated-endpoint/index.js
@@ -0,0 +1,102 @@
+const express = require("express");
+
+const jwt = require("jsonwebtoken");
+
+const app = express();
+
+app.use(express.json());
+
+const users = [];
+
+const JWT_SECRET = "science";
+
+app.post("/signup", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!username || !password){
+        return res.json({
+            message: "Please fill both username and password!",
+        });
+    }
+
+    if (users.find((user) => user.username === username)) {
+        return res.json({
+            message: "You are already signed up!",
+        });
+    }
+
+    if (username.length < 5) {
+        return res.json({
+            message: "You need to have at least 5 users to sign up",
+        });
+    }
+
+    users.push({
+        username: username,
+        password: password,
+    });
+
+    res.json({
+        message: "You have signed up successfully!",
+    });
+});
+
+app.post("/signin", function (req, res) {
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if(!username || !password){
+        return res.json({
+            message: "Please fill both username and password!",
+        });
+    }
+
+    const foundUser = users.find((user) => user.username === username && user.password === password);
+
+    if (foundUser) {
+        const token = jwt.sign(
+            {
+                username: foundUser.username,
+            },
+            JWT_SECRET
+        );
+
+        return res.json({
+            token: token,
+            message: "You have signed in successfully!",
+        });
+    } else {
+        return res.json({
+            message: "Invalid username or password!",
+        });
+    }
+});
+
+app.get("/mystuff", function (req, res) {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.json({
+            message: "Token is missing!",
+        });
+    }
+
+    const userDetails = jwt.verify(token, JWT_SECRET);
+
+    const foundUser = users.find((user) => user.username === userDetails.username);
+
+    if (foundUser) {
+        return res.json({
+            username: foundUser.username,
+            password: foundUser.password,
+            message: "you are authenticated with your authorization!",
+        });
+    } else {
+        return res.json({
+            message: "Invalid token!",
+        });
+    }
+});
+
+app.listen(3000);
